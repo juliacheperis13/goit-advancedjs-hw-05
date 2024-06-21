22c46b1f5a37bd92a891f796ec062d3f6dab9151
Julia Cheperis <julia.cheperis@dataart.com>
2024-06-21 22:35:19 +0200

    add files

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { concatenation } from './concatenation';
+
+const button = document.querySelector('button')!;
+const input = document.querySelector('input')!;
+
+if (button && input) {
+  button.addEventListener('click', () => {
+    console.log('here')
+    concatenation(input.value, 'hello!');
+  });
+}
